85292a678fa3e7512fb9ea571b7f97c7373aa386
indrasyahputra <indrasyahputra048@mail.com>
2021-05-19 12:50:04 +0700

    Manajemen Produk - Membuat Attribute Produk

---
+++ resources/views/admin/attributes/index.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.layout')
+
+@section('content')
+
+<div class="content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card card-default">
+                <div class="card-header card-header-border-bottom">
+                    <h2>Attributes</h2>
+                </div>
+
+                <div class="card-body">
+                    @include('admin.partials.flash')
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Code</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Type</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                        @forelse ($attributes as $attribute)
+                            <tr>
+                                <td scope="row">{{$attribute->id}}</td>
+                                <td>{{$attribute->code}}</td>
+                                <td>{{$attribute->name}}</td>
+                                <td>{{$attribute->type}}</td>
+                                <td>
+                                    <a href="{{ url('admin/attributes/'. $attribute->id .'/edit') }}" class="btn btn-warning btn-sm">Edit</a>
+                                    @if ($attribute->type == 'select')
+                                        <a href="{{ url('admin/attributes/'. $attribute->id .'/options')}}" class="btn btn-success btn-sm">Options</a>
+                                    @endif
+                                    {!! Form::open(['url' => 'admin/attributes/'. $attribute->id, 'class' => 'delete', 'style' => 'display:inline-block']) !!}
+                                    {!! Form::hidden('_method', 'DELETE') !!}
+                                    {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                                    {!! Form::close() !!}
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5">No records found</td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-right">
+                    <a href="{{url('admin/attributes/create')}}" class="btn btn-primary">Add New</a>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
+
+@endsection
